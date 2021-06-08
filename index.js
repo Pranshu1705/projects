47bcc07185aa4338f3c5e0ffcde78af960a22444
Pranshu1705 <78522344+Pranshu1705@users.noreply.github.com>
2021-06-08 22:23:43 +0530

    day/night toggle

---
+++ index.js
@@ -0,0 +1,11 @@
+let body = document.querySelector(".container");
+let toggle = document.getElementById("toggle")
+
+toggle.addEventListener('click', e=> {
+    if(toggle.checked){
+        body.classList.add('dark')}
+        else{
+            body.classList.remove('dark')
+        }
+    }
+);
